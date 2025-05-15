10f3caa3c1bcd75df264a21d7eccaef6087cdde1
keviny2 <kevinyang10@gmail.com>
2025-05-15 13:46:26 -0700

    Add test to convert empty SFE to AnnData

--- tests/test_to_anndata.py
+++ tests/test_to_anndata.py
@@ -1,6 +1,6 @@
 import anndata as ad
 import geopandas as gpd
-from biocframe import BiocFrame
+from spatialfeatureexperiment import SpatialFeatureExperiment
 
 __author__ = "keviny2"
 __copyright__ = "keviny2"
@@ -17,6 +17,13 @@ def test_to_anndata(sfe):
     assert isinstance(obj.uns['spatial']['col_geometries']['spot_polygons'], gpd.GeoDataFrame)
     assert isinstance(obj.uns['spatial']['row_geometries']['gene_points'], gpd.GeoDataFrame)
     assert isinstance(obj.uns['spatial']['annot_geometries']['tissue_boundaries'], gpd.GeoDataFrame)
-    assert isinstance(obj.uns['spatial']['spatial_graphs'], BiocFrame)
+    assert isinstance(obj.uns['spatial']['spatial_graphs'], dict)
     assert obj.uns['spatial']['unit'] == 'full_res_image_pixel'
 
+
+def test_to_anndata_empty():
+    tsfe = SpatialFeatureExperiment()
+
+    obj, alt_exps = tsfe.to_anndata()
+
+    assert isinstance(obj, ad.AnnData)
